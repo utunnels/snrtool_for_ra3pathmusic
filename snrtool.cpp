e640f7400d48b757a83993f90d2a5282ce9fa2dd
utunnels <utunnels@hotmail.com>
2021-12-24 10:36:01 +0800

    Add command line options

--- snrtool.cpp
+++ snrtool.cpp
@@ -1,43 +1,60 @@
 #include "simex.h"
+#include <stdio.h>
+#include <string.h>
 
-int main()
+void testfunction(char * path) {
+	SIMEX_init();
+	int type = SIMEX_id(path, 0);
+	SINSTANCE* instance;
+	SIMEX_open(path, 0, type, &instance);
+	SINSTANCE* instance2;
+	char buf[256];
+	strcpy_s(buf, path);
+	int l = strlen(buf);
+	buf[l - 4] = '\0';
+	strcat_s(buf, ".wav");
+	SIMEX_create(buf, wave, &instance2);
+	SINFO* info;
+	SIMEX_info(instance, &info, 0);
+	if (info)
+	{
+		SIMEX_read(instance, info, 0);
+		info->sound[0]->codec = s16l_int;
+		SIMEX_write(instance2, info, 0);
+		SIMEX_freesinfo(info);
+	}
+	SIMEX_close(instance);
+	SIMEX_wclose(instance2);
+	SIMEX_shutdown();
+}
+
+int main(int argc, char* argv[])
 {
-    SIMEX_init();
-    int type = SIMEX_id("9.snr", 0);
-    SINSTANCE* instance;
-    SIMEX_open("9.snr", 0, type, &instance);
-    SINSTANCE* instance2;
-    SIMEX_create("9.wav", wave, &instance2);
-    SINFO* info;
-    SIMEX_info(instance, &info, 0);
-    if (info)
-    {
-        SIMEX_read(instance, info, 0);
-        info->sound[0]->codec = s16l_int;
-        SIMEX_write(instance2, info, 0);
-        SIMEX_freesinfo(info);
-    }
+	char* infile = argc > 1 ? argv[1] : "input.wav";
+	char* outfile = argc > 2 ? argv[2] : "output";
+	int l = strlen(infile);
+	if (l > 4 && strcmp(infile + l - 4, ".snr")==0) {
+		testfunction(infile);
+		return 0;
+	}
+	SIMEX_init();
+	int type = SIMEX_id(infile, 0);
+	SINSTANCE* instance;
+	SIMEX_open(infile, 0, type, &instance);
+	SINSTANCE* instance2;
+	SIMEX_create(outfile, sndplayer, &instance2);
+	SINFO* info;
+	SIMEX_info(instance, &info, 0);
+	if (info)
+	{
+		SIMEX_read(instance, info, 0);
+		info->sound[0]->codec = s16b_int;// ealayer3_int;
+		info->sound[0]->playloc = PLAYLOC_STREAM;
+		SIMEX_write(instance2, info, 0);
+		SIMEX_freesinfo(info);
+	}
     SIMEX_close(instance);
     SIMEX_wclose(instance2);
     SIMEX_shutdown();
-
-    SIMEX_init();
-    type = SIMEX_id("9.wav", 0);
-    instance;
-    SIMEX_open("9.wav", 0, type, &instance);
-    instance2;
-    SIMEX_create("9out", sndplayer, &instance2);
-    info;
-    SIMEX_info(instance, &info, 0);
-    if (info)
-    {
-        SIMEX_read(instance, info, 0);
-        info->sound[0]->codec = ealayer3_int;
-        info->sound[0]->playloc = PLAYLOC_STREAM;
-        SIMEX_write(instance2, info, 0);
-        SIMEX_freesinfo(info);
-    }
-    SIMEX_close(instance);
-    SIMEX_wclose(instance2);
-    SIMEX_shutdown();
+	return 0;
 }
